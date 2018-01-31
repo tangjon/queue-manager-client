e009e7751457a6e00eb4459758ec22877be1344d
Jonathan Tang <tangjon@hotmail.com>
2018-01-31 00:08:14 -0800

    prototype login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public afAuth: AngularFireAuth) {
   }
-
+  login(username:string, password:string) {
+    console.log(username, password)
+    this.afAuth.auth.signInAndRetrieveDataWithEmailAndPassword(username,password);
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
