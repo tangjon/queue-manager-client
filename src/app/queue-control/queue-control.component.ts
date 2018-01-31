d776dfd7822c64058cb8a814ec320bb88111f546
Tang <jonathan.tang01@sap.com>
2018-01-31 13:08:11 -0800

    acts as a dashboard

--- src/app/queue-control/queue-control.component.ts
+++ src/app/queue-control/queue-control.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../model/user';
 @Component({
   selector: 'app-queue-control',
   templateUrl: './queue-control.component.html',
   styleUrls: ['./queue-control.component.css']
 })
-export class QueueControlComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class QueueControlComponent {
+  itemsRef: AngularFireList<any>;
+  users: Observable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list('users');
+    this.users = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
   }
-
 }
