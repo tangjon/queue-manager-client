06a5e20fffe6715c5ecb28d45081d22dc2b2a748
Tang <jonathan.tang01@sap.com>
2018-01-31 15:26:40 -0800

    make db public

--- src/app/queue-control/queue-control.component.ts
+++ src/app/queue-control/queue-control.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { User } from '../model/user';
 @Component({
@@ -8,12 +8,18 @@ import { User } from '../model/user';
   styleUrls: ['./queue-control.component.css']
 })
 export class QueueControlComponent {
+  itemRef: AngularFireObject<any>;
   itemsRef: AngularFireList<any>;
   users: Observable<any[]>;
-  constructor(db: AngularFireDatabase) {
+  constructor(public db: AngularFireDatabase) {
     this.itemsRef = db.list('users');
     this.users = this.itemsRef.snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
     });
+  }
+
+  toggleAvailability(key, bool) {
+    this.itemRef = this.db.object('users/' + key);
+    this.itemRef.update({ isAvailable: bool })
   }
 }
