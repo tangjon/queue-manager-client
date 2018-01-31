dda1bb67cd54c400c1258f92111b9bf9cd1bbaf2
Tang <jonathan.tang01@sap.com>
2018-01-31 14:49:59 -0800

    add to model

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -2,9 +2,20 @@ export class User {
     iNumber: string;
     name:string;
     key: string;
+    isAvailable: boolean;
+    incidentAmount: number;
+
     constructor(iNumber, name, key){
         this.iNumber = iNumber;
         this.name = name;
         this.key = key;
+        this.isAvailable = true;
+        this.incidentAmount = 0;
+    }
+    checkAvailable(){
+        return this.isAvailable
+    }
+    setAvailable(bool:boolean){
+        this.isAvailable = bool
     }
 }
