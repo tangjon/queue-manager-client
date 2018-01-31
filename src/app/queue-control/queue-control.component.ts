78101aeacf233581f4357a9335666795a89eafb7
Tang <jonathan.tang01@sap.com>
2018-01-31 09:29:36 -0800

    generate q control component

---
+++ src/app/queue-control/queue-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-queue-control',
+  templateUrl: './queue-control.component.html',
+  styleUrls: ['./queue-control.component.css']
+})
+export class QueueControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
