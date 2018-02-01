abc7a7d514622fca32bdfbd049761dfde7e23782
Tang <jonathan.tang01@sap.com>
2018-01-31 16:46:59 -0800

    setup structure of rcc

--- src/app/rcc-management/rcc-management.component.ts
+++ src/app/rcc-management/rcc-management.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-rcc-management',
   templateUrl: './rcc-management.component.html',
   styleUrls: ['./rcc-management.component.css']
 })
-export class RccManagementComponent implements OnInit {
+export class RccManagementComponent {
+  selectedUser: User
 
-  constructor() { }
+  itemsRef: AngularFireList<any>;
+  users: Observable<any[]>;
+  constructor(public db: AngularFireDatabase) {
+    this.itemsRef = db.list('users');
+    this.users = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+  }
 
-  ngOnInit() {
+  selectUser(user){
+    this.selectedUser = user;
+    console.log(user)
   }
 
 }
