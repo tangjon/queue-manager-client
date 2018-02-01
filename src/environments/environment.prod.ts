36e26c4ee5ddeb87e9d97cd86202b2686eae20e4
Tang <jonathan.tang01@sap.com>
2018-02-01 14:02:22 -0800

    reinstall boostrap 4 and change production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCoesP_YkwMmpJaGWO3PO7A9VgjnrrmlxQ",
+    authDomain: "queue-manager-fb-ajs.firebaseapp.com",
+    databaseURL: "https://queue-manager-fb-ajs.firebaseio.com",
+    projectId: "queue-manager-fb-ajs",
+    storageBucket: "queue-manager-fb-ajs.appspot.com",
+    messagingSenderId: "1044152156042"
+  }
 };
