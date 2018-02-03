b3e5e3f7f0f14a686124c4032e53a80bf7912651
Jonathan Tang <tangjon@hotmail.com>
2018-02-03 12:01:55 -0800

    add role model

---
+++ src/app/model/role.ts
@@ -0,0 +1,22 @@
+export class Role {
+    MS: boolean;
+    SA: boolean;
+    SM: boolean;
+    FC_EA_IC_FIM: boolean;
+    DSM: boolean;
+    PCM: boolean;
+    RTC: boolean;
+    LOD_ANA_PL: boolean;
+    NW: boolean;
+    constructor() {
+        this.MS = false;
+        this.SA = false;
+        this.SM = false;
+        this.FC_EA_IC_FIM = false;
+        this.DSM = false;
+        this.PCM = false;
+        this.RTC = false;
+        this.LOD_ANA_PL = false;
+        this.NW = false;
+    }
+}
