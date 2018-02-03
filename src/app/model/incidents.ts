afadb682670809e8da571e3a4ea2785b3ec550c0
Jonathan Tang <tangjon@hotmail.com>
2018-02-03 00:38:56 -0800

    add PCM

--- src/app/model/incidents.ts
+++ src/app/model/incidents.ts
@@ -4,12 +4,14 @@ export class Incidents {
     SM: number;
     FC_EA_IC_FIM: number;
     DSM: number;
+    PCM: number;    
     RTC: number;
     LOD_ANA_PL: number;
     NW: number;
     constructor() {
         this.NW = 0;
         this.MS = 0;
+        this.PCM = 0;        
         this.SA = 0;
         this.SM = 0;
         this.FC_EA_IC_FIM = 0;
