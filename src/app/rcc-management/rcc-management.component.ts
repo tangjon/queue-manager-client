e9addddd6d8a53a4dde3f66d4cd4a72728998d5b
Tang <jonathan.tang01@sap.com>
2018-02-16 16:16:13 -0800

    prototype concept works

--- src/app/rcc-management/rcc-management.component.ts
+++ src/app/rcc-management/rcc-management.component.ts
@@ -10,12 +10,17 @@ import { UserService } from '../services/user.service';
   templateUrl: './rcc-management.component.html',
   styleUrls: ['./rcc-management.component.css']
 })
-export class RccManagementComponent {
+export class RccManagementComponent implements OnInit {
+
   selectedUser: User;
   users: Observable<any[]>;
   showSpinner: boolean = true;
   _userList: Array<User>;
   errorMessage: string;
+
+  currentDate: Date;
+  nextResetDate: Date;
+  lastResetDate: Date;
   constructor(public db: AngularFireDatabase, public userSerivice: UserService) {
     this.userSerivice.getUsers({}).subscribe(r => {
       this.showSpinner = false;
@@ -31,6 +36,10 @@ export class RccManagementComponent {
     })
   }
 
+  ngOnInit(): void {
+    this.currentDate = new Date();
+  }
+
   selectUser(user) {
     this.selectedUser = user;
   }
@@ -41,24 +50,54 @@ export class RccManagementComponent {
       let amount = parseInt(val);
 
       if (this.selectedUser) {
-        let prompt = window.confirm(this.selectedUser.name + " will have " + this.selectedUser.currentQDays + " increased by " + val + " to " +  (this.selectedUser.currentQDays + amount) + ". \nClick okay to confirm.");
+        let prompt = window.confirm(this.selectedUser.name + " will have " + this.selectedUser.currentQDays + " increased by " + val + " to " + (this.selectedUser.currentQDays + amount) + ". \nClick okay to confirm.");
         this.selectedUser.currentQDays += amount;
         this.userSerivice.updateUser(this.selectedUser).subscribe(r => {
-        })}}
+        })
+      }
+    }
   }
 
   updateQueueDays(val) {
     if (val) {
       let amount = parseInt(val);
       if (this.selectedUser) {
-        let prompt = window.confirm(this.selectedUser.name + " will have " + this.selectedUser.currentQDays + " changed to " + val +  ". \nClick okay to confirm.");
+        let prompt = window.confirm(this.selectedUser.name + " will have " + this.selectedUser.currentQDays + " changed to " + val + ". \nClick okay to confirm.");
         this.selectedUser.currentQDays = amount;
-        this.userSerivice.updateUser(this.selectedUser).subscribe(r => {})
+        this.userSerivice.updateUser(this.selectedUser).subscribe(r => { })
       }
     }
   }
 
   clearSelectedUser() {
+
+  }
+
+  // Oct-Dec = 1
+  // Jan-Mar = 2
+  // Apr-Jun = 3
+  // Jul-Sep = 4
+  getQuarter(d) {
+    d = d || new Date(); // If no date supplied, use today
+    var q = [2, 3, 4, 1];
+    return q[Math.floor(d.getMonth() / 3)];
+  }
+  daysLeftInQuarter(d) {
+    d = d || new Date();
+    d.setDate(d.getDate() + 43)
+    var qEnd:any = new Date(d);
+    qEnd.setMonth(qEnd.getMonth() + 3 - qEnd.getMonth() % 3, 0);
+    return Math.floor((qEnd - d) / 8.64e7);
+  }
+
+  getResetDays(){
+    let daysLeft = this.daysLeftInQuarter(new Date());
+    console.log(daysLeft);
+    if(daysLeft == 0){
+      return "RESET NOW"
+    } else {
+      return daysLeft;
+    }
   }
 
 }
