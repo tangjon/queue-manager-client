40e1bf1a541a75abcf20b39bb6e4a52df03b149c
Tang <jonathan.tang01@sap.com>
2018-02-19 16:12:11 -0800

    refactor incident model

--- src/app/model/incidents.ts
+++ src/app/model/incidents.ts
@@ -19,6 +19,25 @@ export class Incidents {
         this.RTC = 0;
         this.LOD_ANA_PL = 0;
     }
+    reset(): void {
+        let object = new Incidents();
+        for (const key in object) {
+            if (object.hasOwnProperty(key)) {
+                object[key] = 0;
+            }
+        }
+    }
 
+    update(update): void {
+        this.NW = 0 || update.NW;
+        this.MS = 0 || update.MS;
+        this.PCM = 0 || update.PCM;
+        this.SA = 0 || update.SA;
+        this.SM = 0 || update.SM;
+        this.FC_EA_IC_FIM = 0 || update.FC_EA_IC_FIM;
+        this.DSM = 0 || update.DSM;
+        this.RTC = 0 || update.RTC;
+        this.LOD_ANA_PL = 0 || update.LOD_ANA_PL;
+    }
 
 }
