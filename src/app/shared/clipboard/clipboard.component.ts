c3fb6a9f9b965d019f1e16f52c311b1f2d1e023f
Tang <jonathan.tang01@sap.com>
2018-02-19 16:36:04 -0800

    beautify

--- src/app/shared/clipboard/clipboard.component.ts
+++ src/app/shared/clipboard/clipboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClipboardComponent implements OnInit {
   clipBoardText = "[EPM_QM_ASSIGNED]"
+  
   constructor() { }
 
   ngOnInit() {
