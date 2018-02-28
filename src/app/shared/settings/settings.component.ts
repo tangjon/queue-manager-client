af8344813b45378f02fc53389c14f7c5ed51afee
tangjon <tangjon@hotmail.com>
2018-02-28 12:04:02 -0800

    Implement Feature: - Overwrite incident counts by type - with snackbar

--- src/app/shared/settings/settings.component.ts
+++ src/app/shared/settings/settings.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {IncidentSetService} from '../../services/incident-set.service';
+import {UserService} from '../../services/user.service';
+import {User} from '../../model/user';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public incidentSetService: IncidentSetService,
+              public  userService: UserService,
+              public  matSnackBar: MatSnackBar) {
+  }
 
   ngOnInit() {
   }
 
+  overwriteIncidentCount(t: NgForm) {
+    if (t.valid) {
+      this.userService.getUser(t.value.iNumber).switchMap((user: User) => {
+        return this.incidentSetService.updateIncidentSet(user, t.value.type, t.value.amount);
+      }).subscribe(() => {
+          this.matSnackBar.open('Success', 'Close', {duration: 1000});
+        },
+        error => {
+          console.log('ERROR!');
+        }
+      );
+    }
+  }
 }
