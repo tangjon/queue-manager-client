2888387c2db1f9fdf18603d3c59ddf89eae545bf
tangjon <tangjon@hotmail.com>
2018-03-02 10:22:33 -0800

    clean up file

--- src/app/shared/clipboard/clipboard.component.ts
+++ src/app/shared/clipboard/clipboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-clipboard',
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clipboard.component.css']
 })
 export class ClipboardComponent implements OnInit {
-  clipBoardText = "[EPM_QM_ASSIGNED]"
-  
-  constructor() { }
+  clipBoardText = '[EPM_QM_ASSIGNED]';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
