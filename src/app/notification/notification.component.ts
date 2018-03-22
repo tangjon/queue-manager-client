68ef558601e7d248d1f286fe476d581b5159bc32
tangjon <tangjon@hotmail.com>
2018-03-22 10:02:54 -0700

    Commit notification files

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {Notification} from "../model/notification";
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent implements OnInit {
+
+  notification_array: Notification[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.notification_array.push(this.getResetNotification());
+  }
+
+  getResetNotification(): Notification {
+    let display = false;
+    if (this.daysLeftInQuarter(new Date) >= 7) {
+      display = true;
+    }
+    return new Notification(
+      "System Info",
+      `There are ${this.daysLeftInQuarter(new Date())} days left in the quarter. Manual reset required soon.`,
+      display
+    );
+  }
+
+  daysLeftInQuarter(d) {
+    d = d || new Date();
+    // d.setDate(d.getDate() + 43)
+    const qEnd: any = new Date(d);
+    qEnd.setMonth(qEnd.getMonth() + 3 - qEnd.getMonth() % 3, 0);
+    return Math.floor((qEnd - d) / 8.64e7);
+  }
+}
