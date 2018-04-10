183447bb2c6bc35c21a55df6dc333c1b0d8dac77
tangjon <tangjon@hotmail.com>
2018-04-10 16:21:22 -0700

    update zone.js test bed for log

--- src/app/core/log.service.spec.ts
+++ src/app/core/log.service.spec.ts
@@ -1,15 +1,25 @@
-import {TestBed, inject} from '@angular/core/testing';
+import {TestBed, inject, async} from '@angular/core/testing';
 import {LogService} from "./log.service";
+import {CoreModule} from "./core.module";
 
 
 describe('LogService', () => {
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     providers: [LogService]
-  //   });
-  // });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CoreModule],
+      providers: [LogService]
+    });
+  });
   //
-  // it('should be created', inject([LogService], (service: LogService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should be created', inject([LogService], (service: LogService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('#getLogs should return all entry logs', async(inject([LogService], (service: LogService) => {
+    service.getLogs().subscribe(result=>{
+      expect(result.length).toBeGreaterThan(0);
+    });
+  })));
+
+
 });
