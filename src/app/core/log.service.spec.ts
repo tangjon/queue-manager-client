7bdee1a46a25a546f419f8f3883fad1c9456f64e
tangjon <tangjon@hotmail.com>
2018-04-10 16:35:56 -0700

    revisit testing for another day

--- src/app/core/log.service.spec.ts
+++ src/app/core/log.service.spec.ts
@@ -1,7 +1,16 @@
 import {TestBed, inject, async} from '@angular/core/testing';
 import {LogService} from "./log.service";
 import {CoreModule} from "./core.module";
+import {User} from "../shared/model/user";
 
+let user = new User({
+  "INUMBER": "i123456",
+  "NAME": "Ahaan",
+  "KEY": "-L6NWB0vTHL_YJzPVshf",
+  "ISAVAILABLE": "true",
+  "USAGEPERCENT": "1",
+  "CURRENTQDAYS": "7"
+});
 
 describe('LogService', () => {
   beforeEach(() => {
@@ -16,10 +25,14 @@ describe('LogService', () => {
   }));
 
   it('#getLogs should return all entry logs', async(inject([LogService], (service: LogService) => {
-    service.getLogs().subscribe(result=>{
+    service.getLogs().subscribe(result => {
       expect(result.length).toBeGreaterThan(0);
     });
   })));
+
+  // it('#addLog should add a log', async(inject([LogService], (service: LogService) => {
+  //   service.addLog(user,"Availability Changed", "Bob(i1232) : 0 to 1 in NW");
+  // })));
 
 
 });
