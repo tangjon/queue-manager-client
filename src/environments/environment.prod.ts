d8da40143998e61dfcd9b582b2008833e4d2bd3c
tangjon <tangjon@hotmail.com>
2018-05-28 09:28:25 -0700

    add tool updates card

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   // Meta Data
   appVersion: 'v2.20',
+  last_updated: 'May 28, 2018',
   appName: 'Queue Manager Cloud Dispatcher',
   appShort: 'QMCD',
   production: true,   // Compile for production: npm run build-prod
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   // Meta Data
   production: false,
   appVersion: 'v2.20',
+  last_updated: 'May 28, 2018',
   appName: 'Queue Manager Cloud Dispatcher',
   appShort: 'QMCD',
 
