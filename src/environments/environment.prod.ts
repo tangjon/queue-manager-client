a5e2e304f62e4f08c77506a3453804b3c445c81d
tangjon <tangjon@hotmail.com>
2018-05-28 09:28:54 -0700

    update versions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // Meta Data
-  appVersion: 'v2.20',
+  appVersion: 'v2.21',
   last_updated: 'May 28, 2018',
   appName: 'Queue Manager Cloud Dispatcher',
   appShort: 'QMCD',
