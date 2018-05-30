8d782f25cb1e74f59d74f516a30bcb2080abcce3
tangjon <tangjon@hotmail.com>
2018-05-30 11:21:08 -0700

    fix dependencies

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -1,1 +1,17 @@
-https://git.hanatrial.ondemand.com/p2000140239trial/queuemanagerdocumentation
+#!/bin/bash
+if [ -d ./documentation  ]; then
+  cp -r ./documentation ../QMDOCS
+	cd ..
+	cd ./QMBUILDPROD
+	git init .
+	git remote add origin https://git.hanatrial.ondemand.com/p2000140239trial/queuemanagerdocumentation
+	git add .
+	git commit -m "Static Deploy"
+	git push origin master --force
+	cd ..
+	rm -rf ./QMDOCS
+else
+   echo run "npm run compodoc" first
+fi
+echo "ctr-z or enter to exit"
+read junk
