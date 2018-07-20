8950fd730f24b0e4be5ff2b10db82e1061e5de8a
Jonathan Tang <tangjon@hotmail.com>
2018-07-19 22:22:40 -0700

    rename folder

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -4,7 +4,7 @@ import {SettingsComponent} from "./settings.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NoticeBoardSettingComponent} from './notice-board-setting/notice-board-setting.component';
 import {ApplicationSettingsComponent} from './application-settings/application-settings.component';
-import {ComponentSettingsComponent} from './component-settings/component-settings.component';
+import {ComponentSettingsComponent} from './product-settings/component-settings.component';
 import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
