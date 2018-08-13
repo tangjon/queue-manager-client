c4307320e7f96c43f82d09a07d80f0f932c2e837
EPM User Production <you@example.com>
2018-08-13 15:32:45 -0700

    change config file

--- qmtoolconfig.ts
+++ qmtoolconfig.ts
@@ -1,4 +1,5 @@
+let VMIP = "http://10.160.202.19:8082" // VIRTUAL MACHINE IP ADDRESS
 export const qmtoolconfig = {
-  api: 'http://10.160.202.19:8082/api',
-  ws_url : 'http://10.160.202.19:8082',
+  api: VMIP + '/api',
+  ws_url : VMIP,
 };
