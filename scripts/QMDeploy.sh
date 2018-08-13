3b3e43357c46c28eae1b412d488b2dc7aaab8058
tangjon <tangjon@hotmail.com>
2018-08-13 16:20:55 -0700

    organize scripts

--- scripts/QMDeploy.sh
+++ scripts/QMDeploy.sh
@@ -4,7 +4,7 @@
 
 # This will bring this project up to date with the repo. All changes to this directory will be lost.
 echo "============================================"
-echo "RUNNING DEPLOYMENT TOOL, DEPLOY TO 'C:\QM TOOL\QM TOOL FRONTEND PROJECT FOLDER\build-prod'"
+echo "RUNNING PRODUCTION DEPLOYMENT TOOL, DEPLOY TO 'C:\QM TOOL\QM TOOL FRONTEND PROJECT FOLDER\build-prod'"
 echo "============================================"
 
 npm run build-prod
